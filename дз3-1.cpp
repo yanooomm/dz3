1577c4743e2b3983f5b78c27e7af00ea7873d2ae
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-10-16 15:52:37 +0500

    Add files via upload

---
+++ дз3-1.cpp
@@ -0,0 +1,45 @@
+﻿ /*TextFile1. Найдите все числа Фибоначчи, попадающие в промежуток от a до b.
+ Ответ записать в файл, располагая по числу в строке. Если таких
+ чисел нет, то результирующий файл должен быть создан, но пуст*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int f1 = 1, f2 = 1, f0;
+    int counter = 0;
+    cin >> a >> b;
+    setlocale(LC_ALL, "ru");
+    ofstream f;
+    f.open("textfile1.txt"); 
+    if (!f.is_open())
+    {
+        cout << "Ошибка открытия файла" << endl;
+    }
+    else
+    {
+        if (f1 == a)
+        {
+            counter += 1;
+            f << a << endl;
+        }
+        while (f2<=b)
+        {
+            f0 = f2; //1
+            f2 = f1 + f2; //2
+            f1 = f0; //1
+            if ((f2 >= a) && (f2 <= b))
+            {
+                counter += 1;
+                f << f2 << endl;
+            }
+        }
+        cout << counter;
+
+    }
+    f.close();
+}
---
+++ дз3-3.cpp
@@ -0,0 +1,31 @@
+﻿/* 1. Написать программу для нахождения корней квадратного уравнения по заданным коэффициентам a, b, c. */
+// тесты находятся в файле test1
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float a, b, c, D, x1, x2;
+	cin >> a >> b >> c;
+	D = b * b - 4 * a * c;
+	if (D > 0)
+	{
+		x1 = ((-b - sqrt(D))) / (2 * a);
+		x2 = (-b + sqrt(D)) / (2 * a);
+		cout << "Первый корень уравнения: " << x1 << endl << "Второй корень уравнения: " << x2;
+	}
+	else
+	{
+		if (D == 0)
+		{
+			x1 = -b / (2 * a);
+			cout << "Корень уравнения: " << x1;
+		}
+		else
+			cout << "Корней нет";
+	}
+}
